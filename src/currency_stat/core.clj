cc35117f8d34895adb540eefe1fb8f17f4c4fbe4
madolu2 <unclemadolu2@gmail.com>
2020-08-05 15:32:27 +0700

    Основной файл. Добавлены константы, теперь в него импортируются функции. Секция работы с бд в процессе разработки

--- src/currency_stat/core.clj
+++ src/currency_stat/core.clj
@@ -1,9 +1,19 @@
 (ns currency-stat.core
-    (:require [currency-stat.request :as req])
-    (:require [currency-stat.db :as db])
-    (:gen-class))
+  (:require [currency-stat.request :as req])
+  (:require [currency-stat.database :as db])
+  (:gen-class))
 
+;const
 (def url "https://www.cbr-xml-daily.ru/daily_json.js")
+
 (def currency-keys [:USD :EUR :CNY :JPY])
 
-(req/get-currency 30 url currency-keys)
+(def days 30)
+
+; db section
+(try (do
+       (db/drop-table)
+       (db/create-db currency-keys))
+     (catch db/create-db currency-keys))
+
+(db/insert-rows (req/get-currency days url currency-keys))
