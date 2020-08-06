8929ba09076e8a54ff07cc1ea57755fde7053c9e
madolu2 <unclemadolu2@gmail.com>
2020-08-06 18:41:52 +0700

    Добавлено форматирование строк

---
+++ src/api/request.clj
@@ -0,0 +1,39 @@
+(ns api.request
+  (:require [clj-http.client :as client])
+  (:require [clojure.data.json :as json]))
+
+(defn nominal-to-one
+  [v n]
+  (float (/ v n)))
+
+; [int string [list of keywords]] -> list of {:day N :CharCode Value} 
+(defn get-currency
+  [days url currency-keys]
+  (loop [days days
+         url url
+         acc []]
+
+    (let [api-map (json/read-str ((client/get url) :body) :key-fn keyword)
+          currency (api-map :Valute)
+          prev-url (str "https:" (api-map :PreviousURL))
+          values (map #(format "%.2f" (nominal-to-one ((currency %) :Value) ((currency %) :Nominal))) currency-keys)]
+
+      (if (zero? days)
+        acc
+        (recur
+         (dec days) prev-url (->> values
+                                  (zipmap currency-keys)
+                                  (into {:date (subs (api-map :Timestamp) 0 10)
+                                         :day days})
+                                  (conj acc)))))))
+
+
+
+
+
+
+
+
+
+
+
