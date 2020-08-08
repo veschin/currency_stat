39b66d5552941f1121b71201dcb6c7d8cb9bafbd
madolu2 <unclemadolu2@gmail.com>
2020-08-08 12:36:39 +0700

    Добавлена функция получения CharCode`s

--- src/api/request.clj
+++ src/api/request.clj
@@ -1,12 +1,22 @@
 (ns api.request
-  (:require [clj-http.client :as client])
-  (:require [clojure.data.json :as json]))
+  (:require [clj-http.client :as client]
+            [clojure.data.json :as json]))
 
 (defn nominal-to-one
   [v n]
   (float (/ v n)))
+(def url "https://www.cbr-xml-daily.ru/daily_json.js")
 
-; [int string [list of keywords]] -> list of {:day N :CharCode Value} 
+; [str] -> [vector of char codes]
+(defn get-char-codes
+  [url]
+  (let [api-map (json/read-str ((client/get url) :body) :key-fn keyword)
+        currency (api-map :Valute)
+        names (map #((currency %) :Name) (keys currency))
+        char-codes (keys currency)]
+    (map vector names char-codes)))
+
+; [int string [list of keywords]] -> [vector of {:day N :CharCode Value}] 
 (defn get-currency
   [days url currency-keys]
   (loop [days days
