4288ebd4e41cdda176483a719cc72dc46f93c374
madolu2 <unclemadolu2@gmail.com>
2020-08-08 12:36:01 +0700

    Удален за ненужностью

--- src/api/interlayer.clj
+++ src/api/interlayer.clj
@@ -1,24 +1,3 @@
-(ns api.interlayer
-  (:require [api.request :as req])
-  (:require [api.database :as db]))
-
-;const
-(def days 30)
-
-(def currency-keys [:USD :EUR :CNY :JPY])
-
-(def url "https://www.cbr-xml-daily.ru/daily_json.js")
-
-(def currency-rows (req/get-currency days url currency-keys))
-
-(defn fill-database
-  [table]
-  (do
-    (db/create-db currency-keys table)
-    (db/insert-rows currency-rows table)))
-
-
-
 
 
 
