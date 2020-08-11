30ee0ca9c97f0e8636f91c2f3bd4ea6c8a4b54ff
madolu2 <unclemadolu2@gmail.com>
2020-08-11 18:42:44 +0700

    Обновлен

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject currency_stat "0.2-preALFA"
+  :plugins [[lein-cljsbuild "1.1.8"]]
   :description "N day currency statistics with data visualization. Clojure - backend. ClojureScript - client side."
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
 
                  ;crb api
@@ -16,8 +14,7 @@
                  [http-kit "2.4.0"]
 
                  ;templates
-                 [hiccup "1.0.5"]
+                 [hiccup "1.0.5"]]
 
-                 ;clientside
-                 [org.clojure/clojurescript "1.10.764"]]
+
   :repl-options {:init-ns server.core})
