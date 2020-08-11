7085aeec25b47602789a79c05b342c0c556b65d3
madolu2 <unclemadolu2@gmail.com>
2020-08-11 18:44:21 +0700

    Рефактор маршрутов. Теперь сервер это атом, который можно перезагружать

--- src/server/core.clj
+++ src/server/core.clj
@@ -1,14 +1,21 @@
 (ns server.core
-  (:require [compojure.core :refer :all]
+  (:require [compojure.core :as ccore]
             [compojure.route :as route]
             [server.views :as views]
             [org.httpkit.server :refer [run-server]]))
 
-(defroutes currency-app
-  (GET "/" [] (views/get-index-page))
-    ; (GET "/currency" [] )
-    ; (POST "/user-query" [] 
-  (route/resources  "/"))
+(defonce server (atom nil))
 
-(defn -main []
-  (run-server currency-app  {:port 5000}))
+(ccore/defroutes currency-app
+  (ccore/GET "/" [] (views/get-index-page))
+  (ccore/POST "/currency" req (views/post-currency req))
+  (route/resources "/"))
+
+
+(defn stop-server []
+  (when-not (nil? @server)
+    (@server :timeout 100)
+    (reset! server nil)))
+
+
+(reset! server (run-server #'currency-app {:port 5010}))
