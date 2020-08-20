4d95b0658c24c4d6cc497d066e43c48ddea982cd
madolu2 <unclemadolu2@gmail.com>
2020-08-20 13:28:12 +0700

    Теперь маршрут /currency принимает конкретные значения, вся валидация в core файле. Теперь сервер - переменная, может перезапускаться.

--- src/server/core.clj
+++ src/server/core.clj
@@ -2,20 +2,25 @@
   (:require [compojure.core :as ccore]
             [compojure.route :as route]
             [server.views :as views]
-            [org.httpkit.server :refer [run-server]]))
+            [org.httpkit.server :refer [run-server]]
+            [clojure.data.json :as json]))
 
 (defonce server (atom nil))
 
 (ccore/defroutes currency-app
   (ccore/GET "/" [] (views/get-index-page))
-  (ccore/POST "/currency" req (views/post-currency req))
+  (ccore/POST "/currency" req
+    (let [req (json/read-str (slurp (req :body)) :key-fn keyword)
+          days (Integer. (req :days))
+          char-codes (map keyword (req :char-codes))] (println days char-codes)
+      (views/get-currency days char-codes)))
   (route/resources "/"))
 
 
 (defn stop-server []
   (when-not (nil? @server)
     (@server :timeout 100)
-    (reset! server nil)))
+    (reset! server (run-server #'currency-app {:port 5020}))))
 
-
-(reset! server (run-server #'currency-app {:port 5010}))
+(defn -main [& args]
+  (reset! server (run-server #'currency-app {:port 5020})))
